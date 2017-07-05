37663bd137be157532a52bb09cadd2b06917c9e0
jorgetinoco <jorge.tinoco@wizeline.com>
2017-07-05 13:37:49 -0500

    add build

--- dist/index.js
+++ dist/index.js
@@ -20,6 +20,10 @@ var _commands = require('./commands');
 
 var _commands2 = _interopRequireDefault(_commands);
 
+var _asyncRulesCommands = require('./asyncRulesCommands');
+
+var _asyncRulesCommands2 = _interopRequireDefault(_asyncRulesCommands);
+
 var _actions = require('./database/actions.js');
 
 var _cache = require('./database/cache.js');
@@ -28,16 +32,6 @@ var _cache2 = _interopRequireDefault(_cache);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/** static data **/
-(0, _actions.getRules)().then(function (result) {
-  _cache2.default.setValue('allRules', result.map(function (rule) {
-    return rule.rule;
-  }));
-  _cache2.default.setValue('rules', result);
-}).then((0, _actions.getMortos)().then(function (result) {
-  _cache2.default.setValue('people', result);
-}));
-
 /** telegram app **/
 var token = process.env.token || _config2.default.get('token');
 var URL = process.env.URL || _config2.default.get('URL');
@@ -45,6 +39,17 @@ var PORT = process.env.PORT || 5000;
 var isDevelopment = process.env.NODE_ENV === 'development';
 
 var app = new _telegraf2.default(token);
+
+/** static data **/
+(0, _actions.getRules)().then(function (result) {
+  _cache2.default.setValue('allRules', result.map(function (rule) {
+    return rule.rule;
+  }));
+  (0, _asyncRulesCommands2.default)(app, result);
+}).then((0, _actions.getMortos)().then(function (result) {
+  _cache2.default.setValue('people', result);
+}));
+
 /** commands **/
 (0, _commands2.default)(app);
 
